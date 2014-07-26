9d1540fd518e31033c113ce44b56e3270dcaf3ad
visualne <dsmith4546@yahoo.com>
2014-07-25 20:15:20 -0400

    added the local repo to /etc/apt/sources.list

--- apache2.pp
+++ apache2.pp
@@ -5,6 +5,10 @@ package { "apache2":
 #Array variable to hold each directory that needs to created.
 $repo_dir = [ "/var/www/debs","/var/www/debs/x86_64"]
 
+#Variable that holds the sed command to be used to add repo to sources.list
+$cmd = "sed -i '1i #Local Repo' /etc/apt/sources.list | sed -i '1i deb http://127.0.0.1/debs/x86_64' /etc/apt/sources.list"
+
+
 #Create directory
 file { $repo_dir:
     require => Package['apache2'],
@@ -13,4 +17,10 @@ file { $repo_dir:
     group  => "root",
     mode   => 755,
 }
-                 
+
+#Adding repo to /etc/apt/sources.list
+exec { "add-repo":
+    require => File[$repo_dir],
+    command => "${cmd}",
+    path    => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games",
+}
