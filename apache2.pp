19a12f92204222ca4a994e0232c84fd706cdf907
visualne <dsmith4546@yahoo.com>
2014-07-27 16:08:22 -0400

    changed some things at the top

--- apache2.pp
+++ apache2.pp
@@ -1,3 +1,7 @@
+##########
+#Check out my blog at: http://visualne.wordpress.com/2014/07/25/create-ubuntu-repository/ for the steps to do this by hand
+##########
+
 #Installing apache
 package { "apache2":
     ensure => "installed"
@@ -11,7 +15,6 @@ package { "dpkg-dev":
 	ensure => "installed"
 }
 
-
 #Array variable to hold each directory that needs to created.
 $repo_dir = [ "/var/www/debs","/var/www/debs/x86_64"]
 
@@ -19,7 +22,7 @@ $repo_dir = [ "/var/www/debs","/var/www/debs/x86_64"]
 $cmd = "sed -i '1i #Local Repo' /etc/apt/sources.list | sed -i '1i deb http://127.0.0.1/debs x86_64' /etc/apt/sources.list"
 
 
-#Create directory
+#Create directory structure
 file { $repo_dir:
     require => Package['apache2'],
     ensure => "directory",
